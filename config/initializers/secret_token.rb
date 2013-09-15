8209d49e0fa2ed2643addd6f1bb86f589a98258e
Mauricio Schneider <me@mauricioschneider.com>
2013-09-15 18:32:14 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photoduel::Application.config.secret_token = '39b54d05e0692f0099687e2554c4aeb149885ffc8fb8b6b737ec83c5aa08508bcdf4b94086de6e27271850e8c753a1ffb6b4323e8d5d2327c6f2c77fa7b68b71'
